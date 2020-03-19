bc5afa87ca07620e7afa6a51f4af28cd473c3cd2
toshihirokato <nanamin.104@icloud.com>
2020-03-19 13:44:07 +0900

    URL のパターンマッチの利用

---
+++ express_test/dice-ex.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+portNo = 3000
+
+// ルートへのアクセス
+app.get('/', (req, res) => {
+  res.send(
+    '<p><a href="/dice/6">6面体のサイコロ</a><br/ >' +
+    '<a href="/dice/12">12面体のサイコロ</a></p>'
+  )
+})
+
+// サイコロへのアクセス
+//! /dice/:num と記述することにより req.params.num としてアクセスできるようになる
+app.get('/dice/:num', (req, res) => {
+  res.send('今回の値は...' + dice(req.params.num))
+})
+
+function dice(n) {
+  return Math.floor(Math.random() * n) + 1
+}
+app.listen(portNo, () => {
+  console.log('起動しました', `http://localhost${portNo}`);
+})
