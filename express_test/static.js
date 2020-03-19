a960ca5eeda2b7411d35b651ef5486cd2e83d695
toshihirokato <nanamin.104@icloud.com>
2020-03-19 15:58:46 +0900

    自動的にファイルを返す

---
+++ express_test/static.js
@@ -0,0 +1,11 @@
+// Expressを起動
+const express = require('express')
+const app = express()
+
+// 待ち受け開始
+app.listen(3000, () => {
+  console.log('起動しました - http://localhost:3000');
+})
+
+// 静的ファイルを自動的に返すように設定
+app.use('/', express.static('./html'))
