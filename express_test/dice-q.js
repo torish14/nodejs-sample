535322cf1c28b508bfc82f4ffdbd9750f7a7bc78
toshihirokato <nanamin.104@icloud.com>
2020-03-19 14:00:40 +0900

    URL パラメーターのクエリー 文字列を取得する

---
+++ express_test/dice-q.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const portNo = 3000
+
+// ルートへのアクセス
+app.get('/', (req, res) => {
+  //! URLパラメーターで取得
+  if (!req.query.q) {
+    res.send(
+      '<p><a href="?q=6">6面体のサイコロ</a><br />' +
+      '<a href="?q=12">12面体のサイコロ</a></p>'
+    )
+  } else {
+    const q = parseInt(req.query.q, 10)
+    res.send(
+      '今回の値は...' + dice(q)
+    )
+  }
+})
+
+function dice(n) {
+  return Math.floor(Math.random() * n) + 1
+}
+
+app.listen(portNo, () => {
+  console.log('起動しました', `http://localhost:${portNo}`);
+
+})
